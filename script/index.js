73fa3a416386a5c04f3ea2326972f928252926bc
ramimahammadarpon <ramim6708@gmail.com>
2025-03-13 22:57:09 +0600

    whole project done

--- script/index.js
+++ script/index.js
@@ -1,6 +1,5 @@
 const removeActive = () =>{
   const actCats = document.getElementsByClassName("active");
-  console.log(actCats);
   for(let actCat of actCats){
     actCat.classList.remove("active");
   }
@@ -31,15 +30,47 @@ const displayCategories = (categories) => {
     }
 }
 
-const loadVideos = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos').then(res => res.json()).then(data => {
+const loadVideos = (value = "") => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${value}`).then(res => res.json()).then(data => {
       const allCat = document.getElementById("btn-all");
       removeActive();
       allCat.classList.add("active");
       displayVideos(data.videos)
     });
 } 
+
+const showVideoDetails = (details) =>{
+  console.log(details);
+  const videoDetails = document.getElementById("video_details");
+  videoDetails.showModal();
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML =`
+    <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src=${details.video.thumbnail}
+      alt="" />
+  </figure>
+  <div class="card-body">
+    <div class="space-y-4">
+      <h2 class="text-3xl font-bold">${details.video.title}</h2>
+      <p class="text-xl">${details.video.description}</p>
+      <p class="text-lg">Author Name : ${details.video.authors[0].profile_name}</p>
+      <p class="font-bold">Views : ${details.video.others.views}</p>  
+    </div>
+  </div>
+</div>
+  `
+  
+}
+
+const loadVideoDetails = (videoId) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+  fetch(url).then(res=> res.json()).then(data=>showVideoDetails(data));
+}
+
 function displayVideos(videos) {
+  console.log(videos)
     const videoContainer = document.getElementById("video-container");
     videoContainer.innerHTML ="";
     if(videos.length == 0) {
@@ -72,11 +103,12 @@ function displayVideos(videos) {
               <div class="space-y-2">
                 <h1 class="font-bold">${video.title}</h1>
                 <p class="flex gap-1 items-center text-gray-400">${video.authors[0].profile_name}
-                    <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">
+                    ${video.authors[0].verified? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></img>`: ``}
                 </p>
                 <p class="text-gray-400">${video.others.views} views</p>
               </div>
             </div>
+            <button onclick=loadVideoDetails("${video.video_id}") class="btn btn-block">Show Details</button>
           </div>
         `
         videoContainer.append(videoCard);
@@ -85,3 +117,8 @@ function displayVideos(videos) {
 
 
 loadCategories();
+
+document.getElementById("search-field").addEventListener("keyup",(event) => {
+  console.log(event.target.value);
+  loadVideos(event.target.value);
+})
